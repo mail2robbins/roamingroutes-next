1694974c1b268590b3e7d7c8b587b4d625d7a405
Robin Thomas <mail2robbins@gmail.com>
2023-05-26 22:13:37 -0400

    added font awesome

--- app/components/SocialLink.js
+++ app/components/SocialLink.js
@@ -1,9 +1,11 @@
 "use client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVideo } from "@fortawesome/free-solid-svg-icons";
 const SocialLink = ({ itemClass, href, icon }) => {
   return (
     <li>
       <a href={href} target="_blank" rel="noreferrer" className={itemClass}>
-        <i className={icon}></i>
+        <FontAwesomeIcon icon={faVideo}></FontAwesomeIcon>
       </a>
     </li>
   );
